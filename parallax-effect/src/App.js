0ca348a93d313fbf9ffcd31b949dd3d01df695dd
AtosHackerMan <atosfalcon@gmail.com>
2023-04-30 17:25:20 -0700

    first commit

---
+++ parallax-effect/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <header className="header-main">
+          <div className="layers">
+            <div className="layer-head">
+              <div className="caption">Welcome to Parallax Web</div>
+              <div className="title">Dark Forest</div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
