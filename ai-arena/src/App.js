88c902d730ff289a8e765f9fa82a1b5d933234b9
AtosHackerMan <atosfalcon@gmail.com>
2023-04-30 17:57:15 -0700

    Second commit

---
+++ ai-arena/src/App.js
@@ -0,0 +1,27 @@
+import layerBaseImage from "./img/layer-base.png";
+import layerMiddleImage from "./img/layer-middle.png";
+import layerFrontImage from "./img/layer-front.png";
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <header className="header-main">
+          <div className="layers">
+            <div className="layer-head">
+              <div className="caption">Welcome to Parallax Web</div>
+              <div className="title">Dark Forest</div>
+            </div>
+            <div className="img-layer layer-base" style={{backgroundImage: `url(${layerBaseImage})`}}></div>
+            <div className="img-layer layer-mid" style={{backgroundImage: `url(${layerMiddleImage})`}}></div>
+            <div className="img-layer layer-front"  style={{backgroundImage: `url(${layerFrontImage})`}}></div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
